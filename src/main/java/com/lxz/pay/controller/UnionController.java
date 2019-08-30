db37e282cb0638f9168ffc683e9b8e68e1b6a2e5
767072965 <767072965@qq.com>
2019-08-30 13:23:03 +0800

    Initial commit

---
+++ src/main/java/com/lxz/pay/controller/UnionController.java
@@ -0,0 +1,12 @@
+package com.lxz.pay.controller;
+
+/**
+ * Created by Administrator on 2019/8/30.
+ * User: lizhen 767072965@qq.com
+ * Date: 2019/8/30
+ * Time: 13:20
+ * Version: pay-service 1.0.0.0
+ * [功能描述]. 银联支付
+ */
+public class UnionController {
+}
