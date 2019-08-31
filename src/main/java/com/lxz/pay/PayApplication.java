40d1d4dbfe95a8a013a3c02234b9814a831a762c
767072965 <767072965@qq.com>
2019-08-31 10:02:00 +0800

    银联支付

---
+++ src/main/java/com/lxz/pay/PayApplication.java
@@ -0,0 +1,20 @@
+package com.lxz.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 2019/8/30.
+ * User: lizhen 767072965@qq.com
+ * Date: 2019/8/30
+ * Time: 13:46
+ * Version: pay-service 1.0.0.0
+ * [功能描述].
+ */
+@SpringBootApplication
+public class PayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayApplication.class, args);
+    }
+}
